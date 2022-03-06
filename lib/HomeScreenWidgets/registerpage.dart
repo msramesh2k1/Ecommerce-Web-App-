835b8ee77af76056330f866abca4373a4e68adc7
Ramesh M S <msramesh2k1@gmail.com>
2022-03-07 01:07:57 +0530

    Register Completed

--- lib/HomeScreenWidgets/registerpage.dart
+++ lib/HomeScreenWidgets/registerpage.dart
@@ -1,8 +1,14 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:mrandmrs_ecom_webapp/Helper.dart';
 import 'package:mrandmrs_ecom_webapp/HomeScreenWidgets/HomeWidgets.dart';
+import 'package:mrandmrs_ecom_webapp/HomeScreenWidgets/accountscree.dart';
 import 'package:mrandmrs_ecom_webapp/Widgets/Custom_Widgets.dart';
 import 'package:mrandmrs_ecom_webapp/constants.dart';
+
+import '../ErrorAlert.dart';
 
 class RegisterScreen extends StatefulWidget {
   const RegisterScreen({Key? key}) : super(key: key);
@@ -12,6 +18,17 @@ class RegisterScreen extends StatefulWidget {
 }
 
 class _RegisterScreenState extends State<RegisterScreen> {
+  FirebaseAuth auth = FirebaseAuth.instance;
+  bool _password = true;
+  final TextEditingController emailcontroller = new TextEditingController();
+  final TextEditingController namecontroller = new TextEditingController();
+  final TextEditingController passwordcontroller = new TextEditingController();
+  @override
+  void initState() {
+    super.initState();
+    _password = false;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -47,19 +64,24 @@ class _RegisterScreenState extends State<RegisterScreen> {
               Container(
                 height: 45,
                 width: 400,
-                child: Row(
-                  children: [
-                    Text(
-                      "   Full Name",
-                      style: GoogleFonts.dmSans(
-                        textStyle: const TextStyle(
-                            fontWeight: FontWeight.normal,
-                            color: Colors.black45,
-                            fontSize: 14,
-                            letterSpacing: 0),
-                      ),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 10.0),
+                  child: TextField(
+                    style: TextStyle(color: kBackgroundColor, fontSize: 14),
+                    cursorColor: kBackgroundColor,
+                    controller: namecontroller,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      focusedBorder: InputBorder.none,
+                      labelText: 'Name',
+                      labelStyle: TextStyle(
+                          fontFamily: 'lato',
+                          fontSize: 14,
+                          fontWeight: FontWeight.normal,
+                          color: Colors.black54),
                     ),
-                  ],
+                  ),
                 ),
                 decoration:
                     BoxDecoration(border: Border.all(color: Colors.black)),
@@ -68,19 +90,24 @@ class _RegisterScreenState extends State<RegisterScreen> {
               Container(
                 height: 45,
                 width: 400,
-                child: Row(
-                  children: [
-                    Text(
-                      "   Email",
-                      style: GoogleFonts.dmSans(
-                        textStyle: const TextStyle(
-                            fontWeight: FontWeight.normal,
-                            color: Colors.black45,
-                            fontSize: 14,
-                            letterSpacing: 0),
-                      ),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 10.0),
+                  child: TextField(
+                    style: TextStyle(color: kBackgroundColor, fontSize: 14),
+                    cursorColor: kBackgroundColor,
+                    controller: emailcontroller,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      focusedBorder: InputBorder.none,
+                      labelText: 'Email',
+                      labelStyle: TextStyle(
+                          fontFamily: 'lato',
+                          fontSize: 14,
+                          fontWeight: FontWeight.normal,
+                          color: Colors.black54),
                     ),
-                  ],
+                  ),
                 ),
                 decoration:
                     BoxDecoration(border: Border.all(color: Colors.black)),
@@ -89,43 +116,73 @@ class _RegisterScreenState extends State<RegisterScreen> {
               Container(
                 height: 45,
                 width: 400,
-                child: Row(
-                  children: [
-                    Text(
-                      "   Password",
-                      style: GoogleFonts.dmSans(
-                        textStyle: const TextStyle(
-                            fontWeight: FontWeight.normal,
-                            color: Colors.black45,
-                            fontSize: 14,
-                            letterSpacing: 0),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 10.0),
+                  child: TextField(
+                    style: TextStyle(color: kBackgroundColor, fontSize: 14),
+                    cursorColor: kBackgroundColor,
+                    controller: passwordcontroller,
+                    obscureText: !_password,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      focusedBorder: InputBorder.none,
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                            _password ? Icons.visibility : Icons.visibility_off,
+                            color: kBackgroundColor,
+                            size: 18),
+                        onPressed: () {
+                          setState(() {
+                            _password = !_password;
+                          });
+                        },
                       ),
+                      labelText: 'Password',
+                      labelStyle: TextStyle(
+                          fontFamily: 'lato',
+                          fontSize: 14,
+                          fontWeight: FontWeight.normal,
+                          color: Colors.black54),
                     ),
-                  ],
+                  ),
                 ),
                 decoration:
                     BoxDecoration(border: Border.all(color: Colors.black)),
               ),
               Box(height: 10, width: 0),
-              Container(
-                height: 45,
-                width: 400,
-                child: Center(
-                  child: Text(
-                    "CREATE ACCOUNT",
-                    style: GoogleFonts.dmSans(
-                      textStyle: const TextStyle(
-                          fontWeight: FontWeight.normal,
-                          color: Colors.white,
-                          fontSize: 14,
-                          letterSpacing: 2),
+              GestureDetector(
+                onTap: () {
+                  emailcontroller.text.isNotEmpty &&
+                          passwordcontroller.text.isNotEmpty &&
+                          namecontroller.text.isNotEmpty
+                      ? _Registerform()
+                      : showDialog(
+                          context: context,
+                          builder: (c) {
+                            return ErrorAlertDialog(message: "Fill Completely");
+                          });
+                },
+                child: Container(
+                  height: 45,
+                  width: 400,
+                  child: Center(
+                    child: Text(
+                      "CREATE ACCOUNT",
+                      style: GoogleFonts.dmSans(
+                        textStyle: const TextStyle(
+                            fontWeight: FontWeight.normal,
+                            color: Colors.white,
+                            fontSize: 14,
+                            letterSpacing: 2),
+                      ),
                     ),
                   ),
+                  decoration: BoxDecoration(
+                      color: Color.fromRGBO(204, 191, 171, 1),
+                      border:
+                          Border.all(color: Color.fromRGBO(204, 191, 171, 1))),
                 ),
-                decoration: BoxDecoration(
-                    color: Color.fromRGBO(204, 191, 171, 1),
-                    border:
-                        Border.all(color: Color.fromRGBO(204, 191, 171, 1))),
               ),
               Box(height: 10, width: 0),
               Row(
@@ -159,5 +216,40 @@ class _RegisterScreenState extends State<RegisterScreen> {
           ),
           color: Colors.white),
     ));
+  }
+
+  Future<void> _Registerform() async {
+    User firebaseUser;
+    await auth
+        .createUserWithEmailAndPassword(
+            email: emailcontroller.text, password: passwordcontroller.text)
+        .then((authUser) {
+      var snackBar = SnackBar(
+          backgroundColor: Colors.white,
+          content: Container(
+            height: 20,
+            child: Center(
+                child: Text(
+              'Account Created Successfully',
+              style: TextStyle(color: Colors.black, letterSpacing: 1),
+            )),
+          ));
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+
+      firebaseUser = authUser.user!;
+      FirebaseFirestore.instance.collection("users").doc(firebaseUser.uid).set({
+        "uid": firebaseUser.uid,
+        "email": firebaseUser.email,
+        "name": namecontroller.text.trim(),
+      });
+      MRANDMRS.sharedprefs!.setString("uid", firebaseUser.uid);
+      MRANDMRS.sharedprefs!.setString("email", firebaseUser.email.toString());
+      MRANDMRS.sharedprefs!
+          .setString("name", namecontroller.text)
+          .then((value) {
+        Route route = MaterialPageRoute(builder: (context) => AccountScreen());
+        Navigator.pushReplacement(context, route);
+      });
+    });
   }
 }
